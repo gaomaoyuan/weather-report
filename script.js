edba8afd9ee5bced27107bb40b5305533f0b7f3f
Maoyuan <>
2023-04-17 15:44:26 -0400

    get resources from weather map and assigned to html

--- script.js
+++ script.js
@@ -54,3 +54,239 @@ $(".searchbtn").click(function () {
   }
 });
 
+function fetch1() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      whichcity +
+      "&appid=1c1db37f109216f4015898ae7bfc7c96"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.cod === "404") {
+        alert("City Not Found");
+        $(".cityname").html("CityName");
+        $(".date0").html("");
+        $(".icon0").attr("src", "");
+        $(".temp0").html("Temp");
+        $(".wind0").html("wind:");
+        $(".hum0").html("humidity:");
+      } else {
+        if (cityhistory.includes(whichcity)) {
+        } else {
+          cityhistory.push(whichcity);
+        }
+
+        localStorage.setItem("dataset", JSON.stringify(cityhistory));
+
+        document.getElementById("fivedays").style.display = "block";
+        $(".cityname").css("font-weight", "bold").html(`${data.name}`);
+        var d = new Date();
+        $(".date0")
+          .css("font-weight", "bold")
+          .html(`(${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()})`);
+        $(".icon0").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp0")
+          .css("font-weight", "bold")
+          .html(`Temp: ${data.main.temp}°F`);
+        $(".wind0")
+          .css("font-weight", "bold")
+          .html(`Wind: ${data.wind.speed}MPH`);
+        $(".hum0")
+          .css("font-weight", "bold")
+          .html(`Humidity: ${data.main.humidity}%`);
+      }
+    });
+
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      whichcity +
+      "&appid=1c1db37f109216f4015898ae7bfc7c96"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.cod === "404") {
+        document.getElementById("fivedays").style.display = "none";
+      } else {
+        document.getElementById("fivedays").style.display = "block";
+
+        $(".date1").html(data.list[7].dt_txt.slice(0, 10));
+        $(".icon1").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[7].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp1").html(`Temp: ${data.list[7].main.temp}°F`);
+        $(".wind1").html(`Wind: ${data.list[7].wind.speed}MPH`);
+        $(".hum1").html(`Humidity: ${data.list[7].main.humidity}%`);
+
+        $(".date2").html(data.list[15].dt_txt.slice(0, 10));
+        $(".icon2").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[15].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp2").html(`Temp: ${data.list[15].main.temp}°F`);
+        $(".wind2").html(`Wind: ${data.list[15].wind.speed}MPH`);
+        $(".hum2").html(`Humidity: ${data.list[15].main.humidity}%`);
+
+        $(".date3").html(data.list[23].dt_txt.slice(0, 10));
+        $(".icon3").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[23].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp3").html(`Temp: ${data.list[23].main.temp}°F`);
+        $(".wind3").html(`Wind: ${data.list[23].wind.speed}MPH`);
+        $(".hum3").html(`Humidity: ${data.list[23].main.humidity}%`);
+
+        $(".date4").html(data.list[31].dt_txt.slice(0, 10));
+        $(".icon4").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[31].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp4").html(`Temp: ${data.list[31].main.temp}°F`);
+        $(".wind4").html(`Wind: ${data.list[31].wind.speed}MPH`);
+        $(".hum4").html(`Humidity: ${data.list[31].main.humidity}%`);
+
+        $(".date5").html(data.list[39].dt_txt.slice(0, 10));
+        $(".icon5").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[39].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp5").html(`Temp: ${data.list[39].main.temp}°F`);
+        $(".wind5").html(`Wind: ${data.list[39].wind.speed}MPH`);
+        $(".hum5").html(`Humidity: ${data.list[39].main.humidity}%`);
+      }
+    });
+}
+function fetch3() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      whichcity +
+      "&appid=1c1db37f109216f4015898ae7bfc7c96"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.cod === "404") {
+        alert("City Not Found");
+        $(".cityname").html("CityName");
+        $(".date0").html("");
+        $(".icon0").attr("src", "");
+        $(".temp0").html("Temp");
+        $(".wind0").html("wind:");
+        $(".hum0").html("humidity:");
+      } else {
+        document.getElementById("fivedays").style.display = "block";
+        $(".cityname").css("font-weight", "bold").html(`${data.name}`);
+        var d = new Date();
+        $(".date0")
+          .css("font-weight", "bold")
+          .html(`(${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()})`);
+        $(".icon0").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp0")
+          .css("font-weight", "bold")
+          .html(`Temp: ${data.main.temp}°F`);
+        $(".wind0")
+          .css("font-weight", "bold")
+          .html(`Wind: ${data.wind.speed}MPH`);
+        $(".hum0")
+          .css("font-weight", "bold")
+          .html(`Humidity: ${data.main.humidity}%`);
+      }
+    });
+
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      whichcity +
+      "&appid=1c1db37f109216f4015898ae7bfc7c96"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.cod === "404") {
+        document.getElementById("fivedays").style.display = "none";
+      } else {
+        document.getElementById("fivedays").style.display = "block";
+
+        $(".date1").html(data.list[7].dt_txt.slice(0, 10));
+        $(".icon1").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[7].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp1").html(`Temp: ${data.list[7].main.temp}°F`);
+        $(".wind1").html(`Wind: ${data.list[7].wind.speed}MPH`);
+        $(".hum1").html(`Humidity: ${data.list[7].main.humidity}%`);
+
+        $(".date2").html(data.list[15].dt_txt.slice(0, 10));
+        $(".icon2").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[15].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp2").html(`Temp: ${data.list[15].main.temp}°F`);
+        $(".wind2").html(`Wind: ${data.list[15].wind.speed}MPH`);
+        $(".hum2").html(`Humidity: ${data.list[15].main.humidity}%`);
+
+        $(".date3").html(data.list[23].dt_txt.slice(0, 10));
+        $(".icon3").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[23].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp3").html(`Temp: ${data.list[23].main.temp}°F`);
+        $(".wind3").html(`Wind: ${data.list[23].wind.speed}MPH`);
+        $(".hum3").html(`Humidity: ${data.list[23].main.humidity}%`);
+
+        $(".date4").html(data.list[31].dt_txt.slice(0, 10));
+        $(".icon4").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[31].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp4").html(`Temp: ${data.list[31].main.temp}°F`);
+        $(".wind4").html(`Wind: ${data.list[31].wind.speed}MPH`);
+        $(".hum4").html(`Humidity: ${data.list[31].main.humidity}%`);
+
+        $(".date5").html(data.list[39].dt_txt.slice(0, 10));
+        $(".icon5").attr(
+          "src",
+          "https://openweathermap.org/img/wn/" +
+            data.list[39].weather[0].icon +
+            "@2x.png"
+        );
+        $(".temp5").html(`Temp: ${data.list[39].main.temp}°F`);
+        $(".wind5").html(`Wind: ${data.list[39].wind.speed}MPH`);
+        $(".hum5").html(`Humidity: ${data.list[39].main.humidity}%`);
+      }
+    });
+}
+
